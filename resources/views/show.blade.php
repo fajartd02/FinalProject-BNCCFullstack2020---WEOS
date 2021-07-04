5241ed96c6729dbfb93cf58f26e7d11fedf47517
fajartd02 <fajarmh2@gmail.com>
2021-07-05 01:54:16 +0700

    Proteksi Edit And Delete

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -20,15 +20,20 @@ Forum
 </div>
 <p>{{$article->subjects}}</p>
 
-<div class="row mb-1">
-    <a href="/post/{{$article->id}}/edit" class = "col-1 btn btn-info btn-sm">Edit</a>
-    <form action="/post/{{$article->id}}" method = "post" class = "col">
-        @csrf
-        @METHOD('DELETE')
+@if (Auth::check())
 
-        <button class="btn btn-sm btn-danger">Delete</button>
-    </form> 
-</div>
+    @if (Auth::user()->id == $article->user_id)
+        <div class="row mb-1">
+        <a href="/post/{{$article->id}}/edit" class = "col-1 btn btn-info btn-sm">Edit</a>
+        <form action="/post/{{$article->id}}" method = "post" class = "col">
+            @csrf
+            @METHOD('DELETE')
+
+            <button class="btn btn-sm btn-danger">Delete</button>
+        </form> 
+        </div> 
+    @endif
+@endif
 
 <a href = "/post" class="btn btn-sm btn-info"><< </a>
 
